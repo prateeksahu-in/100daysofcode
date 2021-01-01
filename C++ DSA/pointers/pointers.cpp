6e244138a06e834c92fb10158030a1ddc535ee95
prateeksahu <prateeksahu0101@gmail.com>
2021-01-01 20:44:15 +0530

    pointer initializations

--- C++ DSA/pointers/pointers.cpp
+++ C++ DSA/pointers/pointers.cpp
@@ -11,6 +11,46 @@ int main()
     cout<<p<<endl;
     //directly print value of variable by pointer
     cout<<*p<<endl;
+    //size of pointer
+    cout<<sizeof(p)<<endl;
+
+    //changes in value of var
+    a++;
+    cout<<a<<endl;
+    cout<<*p<<endl;
+
+    // initializing with var and its pointer
+    cout<<endl;
+    int i=a;
+    int j=*p;
+
+    cout<<i<<endl;
+    cout<<j<<endl;
+
+    j++;
+
+    cout<<j<<endl;
+    cout<<*p<<endl;
+
+    //initializing one pointer with another pointer var
+    int *q=p;
+
+
+    cout<<q<<endl;
+    cout<<p<<endl;
+    //garbage in pointer
+   // int *ptr;
+   // cout<<ptr<<endl;
+  //  cout<<*ptr<<endl;
+  //  *ptr++;
+   // cout<<*ptr<<endl;
+
+
+   //segmentation error(always initialize pointer with null)
+    int *ptr2;
+    cout<<*ptr2<<endl;
+    (*(ptr2+1))++;
+    cout<<*ptr2<<endl;
 
 
 
