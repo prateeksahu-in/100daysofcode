b362d25e0c5e8353e044f0c71cad0b30c579e6c0
prateeksahu <prateeksahu0101@gmail.com>
2021-01-01 20:18:56 +0530

    pointer

--- C++ DSA/pointers/pointers.cpp
+++ C++ DSA/pointers/pointers.cpp
@@ -4,9 +4,21 @@ using namespace std;
 int main()
 {
     int a=10;
-
-    cout<<&a;
+    //print address if var 'a' by '& symbol'
+    cout<<&a<<endl;
     int *p = &a;
-    cout<<endl<<p;
+    //print address of var 'a' stored in pointer
+    cout<<p<<endl;
+    //directly print value of variable by pointer
+    cout<<*p<<endl;
+
+
+
+    float f = 10.2;
+    float *pf=&f;
+
+    double d= 122.32;
+    double *pd =&d;
+
     return 0;
 }
