88d6c7fe8f49abf5b124e32e3c04a7e723488d15
prateeksahu-in <prateeksahu0101@gmail.com>
2021-01-05 21:16:27 +0530

    number-rectangle

---
+++ c++ basic question/Pattern/number_rectangle.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int len;
+    cin>>len;
+    for(int i=1;i<=len;i++)
+    {
+        for(int j=1;j<=len;j++)
+        {
+            cout<<i;
+        }
+        cout<<endl;
+    }
+}
